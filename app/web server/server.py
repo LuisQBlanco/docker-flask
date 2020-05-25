e63db8ce58c11c79afac311d559fd20692e5d0d4
LuisQBlanco <ingluisq@gmail.com>
2020-05-25 09:44:27 -0400

    Try new template

--- app/web server/server.py
+++ app/web server/server.py
@@ -1,5 +1,3 @@
-import os
-from flask import send_from_directory
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
@@ -7,13 +5,26 @@ app = Flask(__name__)
 print(__name__)
 
 
-@app.route('/<username>/<int:post_id>')
-def hello_world(username=None, post_id=None):
-    return render_template('index.html', name=username, post_id=post_id)
+@app.route('/')
+def my_home():
+    return render_template('index.html')
 
 @app.route('/about.html')
 def about():
     return render_template('about.html')
+
+@app.route('/works.html')
+def works():
+    return render_template('works.html')
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/components.html')
+def components():
+    return render_template('components.html')
+
 
 
 # @app.route('/favicon.ico')
